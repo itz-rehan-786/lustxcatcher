50541d0e09b618654342042774eb00228e93a69b
Zoro0003 <aryansaini77260@gmail.com>
2024-08-27 09:31:53 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -13,7 +13,7 @@ from shivu import collection, top_global_groups_collection, group_user_totals_co
 from shivu import application, LOGGER, TOKEN 
 from shivu import set_on_data, set_off_data
 from shivu.modules import ALL_MODULES
-from config import Config
+from config.py import Config
 locks = {}
 message_counters = {}
 spam_counters = {}
