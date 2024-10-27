6706b6f2de67d0367416a8eb8c8d979e8a1f66e8
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-10-27 20:34:53 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "5116239739"
-    sudo_users = ["5116239739"]
-    GROUP_ID = "-1002158051312"
+    OWNER_ID = "7775259302"
+    sudo_users = ["7775259302"]
+    GROUP_ID = "-1002279809564"
     TOKEN = "8076075071:AAF-2RVBTv2uHXX0bAEvoaKRQgcj2PpEc70"
     mongo_url = "mongodb+srv://Slavewaifu:bot@cluster0.sx51gw3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
-    SUPPORT_CHAT = "bwnsnansn67"
-    UPDATE_CHAT = "bwnsnansn67"
-    BOT_USERNAME = "Slave_grabber_bot"
-    CHARA_CHANNEL_ID = "-1002158051312"
+    SUPPORT_CHAT = "ur_hell_paradise"
+    UPDATE_CHAT = "sungupdate"
+    BOT_USERNAME = "sung_gamebot"
+    CHARA_CHANNEL_ID = "-1002279809564"
     api_id = "27744639"
     api_hash = "a5e9da62bcd7cc761de2490c52c89ccf"
 
