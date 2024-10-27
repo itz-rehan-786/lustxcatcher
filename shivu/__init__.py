b108d5b01d4926c9387d1a8058cdeac4577c0b30
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-10-27 20:59:08 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -30,8 +30,8 @@ UPDATE_CHAT = Config.UPDATE_CHAT
 BOT_USERNAME = Config.BOT_USERNAME 
 sudo_users = Config.sudo_users
 OWNER_ID = Config.OWNER_ID 
-JOINLOGS = "-1002041586214"
-LEAVELOGS = "-1001977783984"
+JOINLOGS = "-1002279809564"
+LEAVELOGS = "-1002279809564"
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
