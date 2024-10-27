1d3905f9ad1fb69c61b06ea25c6fe32375e3d882
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-10-27 16:23:26 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "5116239739"
     sudo_users = ["5116239739"]
     GROUP_ID = "-1002158051312"
-    TOKEN = "7246514260:AAGRYq6AIHGjKm1-Vbt5UEgeYEcMRDpDm58"
+    TOKEN = "8076075071:AAF-2RVBTv2uHXX0bAEvoaKRQgcj2PpEc70"
     mongo_url = "mongodb+srv://Slavewaifu:bot@cluster0.sx51gw3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
     SUPPORT_CHAT = "bwnsnansn67"
@@ -14,6 +14,10 @@ class Config(object):
     CHARA_CHANNEL_ID = "-1002158051312"
     api_id = "27744639"
     api_hash = "a5e9da62bcd7cc761de2490c52c89ccf"
+
+    
+
+    
 
     
 class Production(Config):
