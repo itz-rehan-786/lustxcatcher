dd1b7f22c2dfe1b7bfeff2ce079517073e36a1b6
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-10-27 21:00:28 +0530

    Update broadcast.py

--- shivu/modules/broadcast.py
+++ shivu/modules/broadcast.py
@@ -4,7 +4,7 @@ from telegram.ext import CallbackContext, CommandHandler
 from shivu import application, top_global_groups_collection, user_collection
 
 async def broadcast(update: Update, context: CallbackContext) -> None:
-    OWNER_ID = 7011990425
+    OWNER_ID = 7775259302
     
     if update.effective_user.id != OWNER_ID:
         await update.message.reply_text("You are not authorized to use this command.")
